527ba660b3df973a8a94cd1cf76b936913e8bc0a
zhengzehao <zzh@miyuan.com>
2021-04-21 16:25:09 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="container">
+          <h1>我是标题</h1>
+          <p>我是第一段话</p>
+          <p>我是第二段话</p>
+        </div>
+      </div>
   );
 }
 
